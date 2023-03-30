71e84c4c1f299a84ac32eb37b81b579d286a853a
“Your <you@example.com>
2023-03-30 07:58:42 +0700

    code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<template >
+  <div>
+    <section>      
+      <form action="">
+        <table>
+          <tr>
+            <td>Ten dang nhap</td>
+            <td><input type="text" v-model="friend.name"></td>
+          </tr>
 
+          <tr>
+            <td>Mat khau</td>
+            <td><input type="password" v-model="friend.password"></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td><button type="submit" @click="sendData">Dang nhap</button>
+                <button type="reset" @click="clearData">Xoa</button>
+            </td>
+          </tr>
+        </table>
+      </form>
+    <h3>
+      {{ friend }}
+    </h3>
+    </section>
+  </div>
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      friend: {
+        name: "",
+        password: ""
+      }
+    }
+  },
+  methods: {
+    sendData(e) {
+      e.preventDefault();
+      console.log("senndData active !!!");
+
+    axios.post("http://localhost:81/API/api.php", {
+      name: this.friend.name,
+      password: this.friend.password
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+      this.clearData();
+    },
+    clearData() {
+      this.friend.name = "";
+      this.friend.password = "";
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="container">
+        <h3>Đăng nhập</h3>
+        <div class="box-login">
+            <div class="form-group">
+                <label class="label-form">Ten dang nhap</label>
+                <input type="text" class="input-form">
+            </div>
+            <div class="form-group">
+                <label class="label-form">Mat khau</label>
+                <input type="password" class="input-form">
+            </div>
+        </div>
+        <div class="button">
+            <div>
+                <button class="btn-orange">
+                    <span>Dang ky</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name : 'Home',
+};
+</script>
+<style scoped>
+.container {
+    width: 300px;
+    height: 300px;
+}
+h3 {
+    font-family: Cabin;
+    font-size: 40px;
+    line-height: 49px;
+    font-weight: normal;
+    margin-bottom: 24px;
+    color: #383F47;
+}
+.box-login {
+    background: #eb6f27;
+    border-radius: 12px;
+    padding: 16px;
+    margin-bottom: 16px;
+}
+.label-form {
+    font-family: Cabin;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 19px;
+    color: #0631a0;
+}
+.input-form {
+    background: #FFFFFF;
+    border: 1px solid #ECEDF1;
+    box-sizing: border-box;
+    border-radius: 12px;
+    font-size: 16px;
+    line-height: 19px;
+    padding: 14px 16px 15px 16px;
+    width: 100%;
+}
+.button {
+    display: flex;
+   justify-content: space-between;
+}
+
+.btn-orange {
+    color: #fff;
+    background: #F39B13;
+    border-radius: 12px;
+    font-family: Cabin;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 22px;
+    text-align: center;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    padding: 17px 24px;
+    border: none;
+    cursor: pointer;
+}
+
+.btn-orange:hover {
+    opacity: 0.7;
+}
+
+a {
+    color: #007bff;
+    text-decoration: none;
+    background-color: transparent;
+}
+</style>
